67deda21a463e90891970d1eabd8e635ef476d1f
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2025-06-09 12:05:40 +0700

    feat(dashboard): setup dashboard worker

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import { ExpressAdapter } from '@bull-board/express';
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { emailQueue } from "../jobs/email/queue";
 import articleRouter from "../routes/article.route";
 
 dotenv.config();
@@ -8,6 +12,22 @@ dotenv.config();
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
+
+// Bull Board setup
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath('/admin/queues');
+
+createBullBoard({
+  queues: [
+    new BullMQAdapter(emailQueue),
+    // Tambahkan queue lain di sini
+  ],
+  serverAdapter,
+});
+
+app.use('/admin/queues', serverAdapter.getRouter());
+
+// Route registration
 app.use('/api/v1', articleRouter);
 
 
